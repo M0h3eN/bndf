3eb00031fa000215dca7e563e9f79f30be4c1517
mohsen hadianpour <mohsenhadianpour@gmail.com>
2020-01-02 05:32:44 +0330

    Add Map[String, Any] type conversion for json structure

---
+++ src/main/scala/ir/ipm/bcol/commons/M2eeJsonProtocol.scala
@@ -0,0 +1,20 @@
+package ir.ipm.bcol.commons
+
+
+import spray.json._
+
+object M2eeJsonProtocol extends DefaultJsonProtocol {
+
+  implicit object MapJsonFormat extends JsonFormat[Map[String, Any]] { // 1
+    def write(m: Map[String, Any]): JsObject = {
+      JsObject(m.mapValues {                  // 2
+        case v: String => JsString(v)         // 3
+        case v: Int => JsNumber(v)
+        case v: Map[_, _] => write(v.asInstanceOf[Map[String, Any]])  // 4
+        case v: Any => JsString(v.toString)   // 5
+      })
+    }
+
+    def read(value: JsValue) = ???            // 6
+  }
+}
