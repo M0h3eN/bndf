d61a2644c042d682c842fdba46c79b0bcadccbff
mohsen hadianpour <mohsenhadianpour@gmail.com>
2020-01-03 16:56:26 +0330

    Test raw signal data structure

--- src/main/scala/ir/ipm/bcol/RecordingDataLoader.scala
+++ src/main/scala/ir/ipm/bcol/RecordingDataLoader.scala
@@ -5,21 +5,25 @@ import spray.json._
 import ir.ipm.bcol.commons.M2eeJsonProtocol.MapJsonFormat
 
 import us.hebi.matlab.mat.format.Mat5.readFromFile
-import ir.ipm.bcol.structure.MetaDataStructureCreator.metaDataStructureCreator
+import ir.ipm.bcol.structure.SchemaCreator
 /**
- * @author ${user.name}
+ * @author ${Mohsen.Hadianpour}
  */
 object RecordingDataLoader {
 
   def main(args : Array[String]) {
 
+    val schemaObject = new SchemaCreator
+
     val matTestFile = readFromFile("/volumes/data/raw-data/TestData/channelik0_1.mat")
     val entries = matTestFile.getEntries.asScala
 
-    val metaData = metaDataStructureCreator(entries, matTestFile)
-
+    val metaData = schemaObject.metaDataSchemaCreator(entries, matTestFile)
     val metaDataJson = metaData.toJson
     println(metaDataJson)
+
+    val rawData = schemaObject.dataSchemaCreator(entries, matTestFile)
+    rawData.foreach(println)
 
   }
 
