ef7d4f9d73401380ceb347065dbfac752f60dd8f
mohsen hadianpour <mohsenhadianpour@gmail.com>
2020-01-10 09:39:04 +0330

    Create MongoConnector.checkIfItExist to set checkpoint in mongoDB Change MongoConnector class to case class to get sparkSession, MongoUri and MongoDataBase at initialize time.

--- src/main/scala/ir/ipm/bcol/commons/MongoConnector.scala
+++ src/main/scala/ir/ipm/bcol/commons/MongoConnector.scala
@@ -2,21 +2,41 @@ package ir.ipm.bcol.commons
 
 import com.mongodb.spark.MongoSpark
 import com.mongodb.spark.config.{ReadConfig, WriteConfig}
+import ir.ipm.bcol.evaluator.ExperimentMetaDataEvaluator
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
-class MongoConnector {
+import scala.util.Try
 
-  def Reader(spark: SparkSession,  uri: String, database: String, collection: String) :DataFrame = {
+case class MongoConnector(spark: SparkSession,  uri: String, database: String) {
+
+
+  def Reader(collection: String): DataFrame = {
 
     val readConfig = ReadConfig(Map("uri" -> uri, "database" -> database, "collection" -> collection))
     MongoSpark.load(spark, readConfig)
 
   }
 
-  def Writer(spark: SparkSession, DF: DataFrame,  uri: String, database: String, collection: String) :Unit = {
+  def Writer(collection: String, DF: DataFrame) :Unit = {
 
     val writeConfig = WriteConfig(Map("uri" -> uri, "database" -> database, "collection" -> collection))
     MongoSpark.save(DF, writeConfig)
   }
 
+
+  def checkIfItExist(collection: String, column: String, experimentList: Array[String]): ExperimentMetaDataEvaluator ={
+
+    val metaCollection = Reader(collection)
+    val meta: Try[Array[String]] = Try(metaCollection.select(column).collect().map(row => row(0).toString))
+    val existedMetaData =  meta.getOrElse(Array("NotExists"))
+
+    val nonExistenceExperiment = existedMetaData.apply(0) match {
+      case "NotExists" => experimentList
+      case _ => experimentList.diff(existedMetaData)
+
+    }
+
+    ExperimentMetaDataEvaluator(nonExistenceExperiment)
+
+  }
 }
