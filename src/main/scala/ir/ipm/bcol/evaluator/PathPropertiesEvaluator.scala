e5c905ed5df6512ea5703e03009d279b7d7b4e8a
mohsen hadianpour <mohsenhadianpour@gmail.com>
2020-01-10 09:40:37 +0330

    Create PathPropertiesEvaluator to get directory and naming properties

---
+++ src/main/scala/ir/ipm/bcol/evaluator/PathPropertiesEvaluator.scala
@@ -0,0 +1,5 @@
+package ir.ipm.bcol.evaluator
+
+case class PathPropertiesEvaluator(pathLen: Int, parentPath: String,
+                                   experimentName: String, channelFileNames: Array[String],
+                                   eventFileNames: Array[String])
