0f5b9b30f94b8e0535d2651480eb4f9138aa4585
mohsen hadianpour <mohsenhadianpour@gmail.com>
2020-02-07 12:12:51 +0330

    Some refactor

--- src/main/scala/ir/ipm/bcol/structure/CharArrayStructure.scala
+++ src/main/scala/ir/ipm/bcol/structure/CharArrayStructure.scala
@@ -6,9 +6,13 @@ trait CharArrayStructure extends CharStructure {
 
   def parseCharArrayToMap(structField: Struct, parentName: String, rowCount: Int): Map[String, String] ={
 
-    val charMapArray = (1 until rowCount).map(elem => parseCharToMap(structField.getChar(parentName, elem), parentName).getOrElse(Map(parentName -> ""))).toArray
+    val charMapArray = (1 until rowCount)
+      .map(elem => parseCharToMap(structField.getChar(parentName, elem), parentName).getOrElse(Map(parentName -> "")))
+      .toArray
     charMapArray.map(_.toSeq).reduce(_ ++ _).groupBy(_._1).mapValues(_.map(_._2).toArray
-      .map(x => x.toString.concat(", ")).foldLeft("[")((x, y) => x + y).foldRight("]")((x, y) => x + y)
+      .map(x => x.toString.concat(", "))
+      .foldLeft("[")((x, y) => x + y)
+      .foldRight("]")((x, y) => x + y)
       .replace(", ]", "]"))
   }
 
