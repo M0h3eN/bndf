f57473d218ccd729915e48c0f406ac1d5083ed5a
M0hSeN <mohsenhadianpour@gmail.com>
2020-07-11 21:04:12 +0430

    Optimize Table Writing

--- src/main/scala/com/ipm/nslab/bdns/SimpleSorter.scala
+++ src/main/scala/com/ipm/nslab/bdns/SimpleSorter.scala
@@ -1,7 +1,9 @@
 package com.ipm.nslab.bdns
 
+import java.util.concurrent.TimeUnit
+
+import com.ipm.nslab.bdns.commons.io.SparkWriter
 import com.ipm.nslab.bdns.commons.{MongoConnector, SparkConfig}
-import com.ipm.nslab.bdns.spark.DataIngestion
 import com.ipm.nslab.bdns.spark.analysis.Sorting
 import com.typesafe.scalalogging.Logger
 import org.apache.spark.sql.SaveMode
@@ -13,6 +15,7 @@ object SimpleSorter {
   val logger: Logger = Logger(s"${this.getClass.getName}")
 
   val sparkConfig = new SparkConfig
+  val sparkWrite = new SparkWriter
   val HIVE_DB = "sortedDataDB"
 
   val MONGO_DB_NAME = "MetaDataDB"
@@ -28,17 +31,21 @@ object SimpleSorter {
     val MONGO_URI_DEFAULT = "mongodb://root:ns123@mongos:27017/admin"
     val MONGO_URI = Try(args.apply(1)).getOrElse(MONGO_URI_DEFAULT)
     val sorting = new Sorting
-    val conf = sparkConfig.sparkInitialConf("Recording Data Loader", MONGO_URI, MONGO_DB_NAME, numberOfSqlPartition)
+    val conf = sparkConfig.sparkInitialConf("Spike Sorter", MONGO_URI, MONGO_DB_NAME, numberOfSqlPartition)
     val spark = sparkConfig.sparkSessionCreator(conf)
     val mongoConnector = MongoConnector(spark, MONGO_URI, MONGO_DB_NAME)
 
     if (!spark.catalog.databaseExists(HIVE_DB)) spark.sql(s"CREATE DATABASE $HIVE_DB")
 
-
     val metaData = mongoConnector.Reader(ex)
-    val sortedDataset = sorting.sorter(spark, metaData)
+    val sortedDataset = sorting.sorter(spark, metaData, ex)
 
-    sortedDataset.write.mode(saveMode = SaveMode.Overwrite).saveAsTable(HIVE_DB + "." + "SORTED")
+    sparkWrite.writeHiveTable(sortedDataset, SaveMode.Overwrite, HIVE_DB, "SORTED",
+      "RecordLocation")
+
+    logger.info("Finished Sorting Spike Trains")
+    TimeUnit.SECONDS.sleep(5)
+    spark.close()
 
   }
 }
