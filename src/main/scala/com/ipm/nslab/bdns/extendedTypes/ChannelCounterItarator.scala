221283aaafd01557413bdcfd350b45b2cffb673b
M0hSeN <mohsenhadianpour@gmail.com>
2020-07-29 02:06:36 +0430

    Add ChannelCounters and ChannelCounterIterator type classes for managing ordering

---
+++ src/main/scala/com/ipm/nslab/bdns/extendedTypes/ChannelCounterItarator.scala
@@ -0,0 +1,4 @@
+package com.ipm.nslab.bdns.extendedTypes
+
+case class ChannelCounterIterator(fullPath: String, channelFileName: String, channelFileInfo: String,
+                                  mainCounter: Int, subCounter: Int)
---
+++ src/main/scala/com/ipm/nslab/bdns/extendedTypes/ChannelCounters.scala
@@ -0,0 +1,3 @@
+package com.ipm.nslab.bdns.extendedTypes
+
+case class ChannelCounters(mainCounter: Int, subCounter: Int)
