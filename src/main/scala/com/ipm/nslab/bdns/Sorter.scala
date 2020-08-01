a2d9f653b2c785d9e9fe4b7a85be4777bd79ae34
M0hSeN <mohsenhadianpour@gmail.com>
2020-08-01 21:21:55 +0430

    Add option to sort all experiments

--- src/main/scala/com/ipm/nslab/bdns/Sorter.scala
+++ src/main/scala/com/ipm/nslab/bdns/Sorter.scala
@@ -30,9 +30,8 @@ object Sorter {
 
     if(args.isEmpty) logger.error("", throw new Exception("Experiment name must specified"))
 
-    val ex = args.apply(0)
     val MONGO_URI_DEFAULT = "mongodb://root:ns123@mongos:27017/admin"
-    val MONGO_URI = Try(args.apply(1)).getOrElse(MONGO_URI_DEFAULT)
+    val MONGO_URI = Try(args.apply(0)).getOrElse(MONGO_URI_DEFAULT)
     val sorting = new Sorting
     val conf = sparkConfig.sparkInitialConf("Spike Sorter", MONGO_URI, MONGO_DB_NAME, numberOfSqlPartition)
     val spark = sparkConfig.sparkSessionCreator(conf)
@@ -43,21 +42,50 @@ object Sorter {
 
     if (!spark.catalog.databaseExists(HIVE_DB)) spark.sql(s"CREATE DATABASE $HIVE_DB")
 
-    val metaData = mongoConnector.Reader(ex)
-    var startTime = System.currentTimeMillis()
-    val sortedDataset = sorting.sorter(spark, metaData, ex)
-    benchmark.WriteBenchmarkData(this.getClass.getName, ex,
-      "CompletionOfExperimentSorting", System.currentTimeMillis() - startTime)
+    val exList = mongoConnector.Reader("Experiments").select("_id").collect().map(_.getAs[String](0))
+    val ex = Try(args.apply(1))
 
-    startTime = System.currentTimeMillis()
-    sparkWrite.writeParquetInHdfs(sortedDataset, SaveMode.Overwrite, MAIN_SORTED_DATA_DIR + "/" + ex + ".parquet")
-    benchmark.WriteBenchmarkData(this.getClass.getName, ex,
-      "CompletionOfWritingSortedData", System.currentTimeMillis() - startTime)
+    if(ex.isSuccess) {
+      val metaData = mongoConnector.Reader(ex.get)
+      val startTime = System.currentTimeMillis()
+      val sortedDataset = sorting.sorter(spark, metaData, ex.get)
+      benchmark.WriteBenchmarkData(this.getClass.getName, ex.get,
+        "CompletionOfExperimentSorting", System.currentTimeMillis() - startTime)
 
-    logger.info("Finished Sorting Spike Trains")
-    hdfsOperator.deleteFileBasedOnDir(SPARK_CHECKPOINT_DIR)
-    TimeUnit.SECONDS.sleep(5)
-    spark.close()
+      val startWTime = System.currentTimeMillis()
+      sparkWrite.writeParquetInHdfs(sortedDataset, SaveMode.Overwrite, MAIN_SORTED_DATA_DIR + "/" + ex + ".parquet")
+      benchmark.WriteBenchmarkData(this.getClass.getName, ex.get,
+        "CompletionOfWritingSortedData", System.currentTimeMillis() - startWTime)
+
+      logger.info("Finished Sorting Spike Trains")
+      hdfsOperator.deleteFileBasedOnDir(SPARK_CHECKPOINT_DIR)
+      TimeUnit.SECONDS.sleep(5)
+      spark.close()
+
+    } else {
+      exList.foreach(ex => {
+
+        val metaData = mongoConnector.Reader(ex)
+        val startTime = System.currentTimeMillis()
+        val sortedDataset = sorting.sorter(spark, metaData, ex)
+        benchmark.WriteBenchmarkData(this.getClass.getName, ex,
+          "CompletionOfExperimentSorting", System.currentTimeMillis() - startTime)
+
+        val startWTime = System.currentTimeMillis()
+        sparkWrite.writeParquetInHdfs(sortedDataset, SaveMode.Overwrite, MAIN_SORTED_DATA_DIR + "/" + ex + ".parquet")
+        benchmark.WriteBenchmarkData(this.getClass.getName, ex,
+          "CompletionOfWritingSortedData", System.currentTimeMillis() - startWTime)
+
+        logger.info(s"Finished Sorting Spike Trains of experiment $ex")
+
+        spark.catalog.clearCache()
+
+      })
+
+      hdfsOperator.deleteFileBasedOnDir(SPARK_CHECKPOINT_DIR)
+      TimeUnit.SECONDS.sleep(5)
+      spark.close()
+    }
 
   }
 }
